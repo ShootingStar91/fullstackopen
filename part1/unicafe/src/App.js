fc9a687514a52412dfc8ce6262996a996edaa6f8
arttu <ttu91@hotmail.com>
2021-08-10 20:34:21 +0300

    1.7 tehty

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,18 @@
 import React, { useState } from 'react'
 
+const Statistics = (props) => {
+  const total = props.good + props.neutral + props.bad
+  const avg_points = (props.good - props.bad) / total
+  const positive = props.good / total * 100
+  return (
+    <div>
+      <p>All: {total}</p>
+      <p>Average: {avg_points}</p>
+      <p>Positive: {positive} %</p>
+    </div>
+  )
+}
+
 const Display = (props) => (
   <div>
     <p>Good: {props.good}</p>
@@ -27,6 +40,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text="Bad"/>
       <Display good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
