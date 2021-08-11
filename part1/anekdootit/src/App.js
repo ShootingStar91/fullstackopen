279bc3951fdf9c0cd19d80e335b21aa9538c0bd9
arttu <ttu91@hotmail.com>
2021-08-11 12:50:07 +0300

    1.6 to 1.14 done

---
+++ part1/anekdootit/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.clickHandler}>{props.text}</button>
+  )
+}
+
+const DisplayVotes = (props) => {
+  let voteAmount = props.votes[props.selected]
+  return (
+    <p>Ääniä: {voteAmount}</p>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+  const zero_votes = [0, 0, 0, 0, 0, 0, 0]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(zero_votes)
+  const mostVoted = function(votes) {
+    return votes.indexOf(Math.max.apply(null, votes))
+  }
+
+
+  console.log("Selected: " + selected)
+  console.log("Votes: ")
+  console.log(votes)
+  return (
+    <div>
+      <h2>Satunnainen anekdootti</h2>
+      <p>{anecdotes[selected]}</p>
+      <p><Button clickHandler={() => setSelected(Math.floor(Math.random() * 7))} text="Uusi anekdootti"/></p>
+      <DisplayVotes votes={votes} selected={selected}/>
+      <p><Button clickHandler={() => {
+          let new_votes = [...votes]
+          new_votes[selected] += 1
+          setVotes(new_votes)
+        }
+        } text="Äänestä"/></p>
+      <h2>Suosituin anekdootti</h2>
+      <p>{anecdotes[mostVoted(votes)]}</p>
+    </div>
+  )
+}
+
+export default App
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,11 +4,18 @@ const Statistics = (props) => {
   const total = props.good + props.neutral + props.bad
   const avg_points = (props.good - props.bad) / total
   const positive = props.good / total * 100
+  if (total === 0) {
+    return <div><p>No feedback given</p></div>
+  }
   return (
     <div>
-      <p>All: {total}</p>
-      <p>Average: {avg_points}</p>
-      <p>Positive: {positive} %</p>
+          <table>
+            <tbody>
+          <StatisticLine name="All" value={total}/>
+          <StatisticLine name="Average" value={avg_points}/>
+          <StatisticLine name="Positive" value={positive}/>
+          </tbody>
+        </table>
     </div>
   )
 }
@@ -18,9 +25,21 @@ const Display = (props) => (
     <p>Good: {props.good}</p>
     <p>Neutral: {props.neutral}</p>
     <p>Bad: {props.bad}</p>
-    
   </div>
 )
+
+const StatisticLine = (props) => {
+  let valuetoprint = "" + props.value
+  if (props.name === "Positive") {
+    valuetoprint = props.value + " %"
+  }
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>{valuetoprint}</td>
+    </tr>
+  )
+}
 
 const Button = (props) => (
   <button onClick={props.handleClick}>
@@ -36,10 +55,13 @@ const App = () => {
 
   return (
     <div>
+      <h2>Give feedback!</h2>
       <Button handleClick={() => setGood(good + 1)} text="Good"/>
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral"/>
       <Button handleClick={() => setBad(bad + 1)} text="Bad"/>
       <Display good={good} neutral={neutral} bad={bad} />
+      <h2>Statistics</h2>
+
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
