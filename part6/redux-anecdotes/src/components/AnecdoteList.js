4ed1a554777ae8e2fc53dd36e598c5e4cb7813dc
arttu <ttu91@hotmail.com>
2021-09-12 23:30:48 +0300

    Part 6 all done and fixed now too

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { vote, sortByVotes } from '../reducers/anecdoteReducer'
+import { setNotification } from '../reducers/notificationReducer'
+import { connect } from 'react-redux'
+
+
+const AnecdoteList = (props) => {
+
+  const voteAnecdote = async (anecdote) => {
+    props.vote(anecdote)
+    props.setNotification('You voted for "' + anecdote.content + '"', 5)
+  }
+
+  const anecdotes = () => {
+    if (props.filter === '') {
+      return props.anecdotes
+    } else {
+      return props.anecdotes.filter((dote) => dote.content.toLowerCase().includes(props.filter.toLowerCase()))
+    }
+  }
+
+  sortByVotes(anecdotes())
+
+  return (
+    <div>
+      {anecdotes().map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => voteAnecdote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}    
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    anecdotes: state.anecdotes,
+    filter: state.filter
+  }
+}
+
+const ConnectedAnecdoteList = connect(mapStateToProps, { setNotification, vote })(AnecdoteList)
+
+export default ConnectedAnecdoteList
---
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,74 @@
+import anecdotesService from '../services/anecdotes'
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+export const sortByVotes = (anecdotes) => {
+
+  return anecdotes.sort((prev, next) => {
+    const diff = prev.votes - next.votes
+    if (diff > 0) {
+      return -1
+    } else if (diff < 0) {
+      return 1
+    }
+    return 0
+  })
+}
+
+
+const anecdoteReducer = (state = [], action) => {
+
+  switch(action.type) {
+    case 'VOTE_ANECDOTE':
+      return state.map((anecdote) => {if (anecdote.id === action.data.id) {
+        return(action.data)
+      } else { return(anecdote)}})
+    case 'ADD_ANECDOTE':
+      return [...state, action.data]
+    case 'INITIALIZE_ANECDOTES':
+      return action.data
+    default:
+      return state
+  }
+}
+
+export const vote = (anecdote) => {
+  anecdote = {...anecdote, votes: anecdote.votes + 1}
+  return async dispatch => {
+    await anecdotesService.updateAnecdote(anecdote)
+    dispatch ({
+      type: 'VOTE_ANECDOTE',
+      data: anecdote
+    })
+  }
+}
+
+export const addAnecdote = (content) => {
+  return async dispatch => {
+    const newAnecdote = await anecdotesService.addAnecdote(content)
+    dispatch ({
+      type: 'ADD_ANECDOTE',
+      data: newAnecdote
+    })
+  }
+}
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdotesService.getAll()
+    dispatch({
+      type: 'INITIALIZE_ANECDOTES',
+      data: anecdotes
+    })
+  }
+}
+
+export default anecdoteReducer
